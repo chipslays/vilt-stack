6604184f9f6e7f238524dd81a404b08446d20d9f
chipslays <hello@chipslays.ru>
2022-05-14 13:12:51 +0400

    fix cp command

--- install.php
+++ install.php
@@ -2,29 +2,29 @@
 
 run(
     'Installing Composer dependencies...',
-    fn () => system('composer install'),
+    'composer install',
 );
 
 run(
     'Installing NPM dependencies...',
-    fn () => system('npm install'),
+    'npm install',
 );
 
 run(
     'Copying .env file...',
-    fn () => system('cp .env.example .env'),
+    (PHP_OS_FAMILY == 'Windows' ? 'copy' : 'cp') . ' .env.example .env',
 );
 
 run(
     'Generating Laravel application key...',
-    fn () => system('php artisan key:generate'),
+    'php artisan key:generate'
 );
 
 run(str_repeat('*', 32));
 run('✨ Your VILT stack project is ready.');
 run(str_repeat('*', 32));
 
-function run(string $message, callable $callback = null) : void {
+function run(string $message, string $command = null) : void {
     echo $message . PHP_EOL;
-    call_user_func($callback ?? fn () => null);
+    if ($command) exec($command);
 }
